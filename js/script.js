5a13d179b4d942ce7a834b3b603998c409cffeaa
MaxTrainingRep <mxgryn@gmail.com>
2022-09-08 11:47:02 +0300

    Added fetch

--- js/script.js
+++ js/script.js
@@ -147,7 +147,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (window.pageYOffset + document.documentElement.clientHeight >=
         document.documentElement.scrollHeight-1) {
             openModal();
-            window.removeEventListener('scroll', showModalByScroll)
+            window.removeEventListener('scroll', showModalByScroll);
         } 
     }
 
@@ -256,10 +256,6 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form); 
 
             const object = {};
@@ -267,19 +263,22 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key]= value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+                .then(data => data.text())
+                .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
@@ -308,9 +307,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
         }, 4000);
     }
-
     ////////////////////////////////////////////////
 
-
+ 
+    
 
 });
